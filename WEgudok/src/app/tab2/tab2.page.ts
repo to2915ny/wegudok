1f12f7f051bf2984c437a683b859c02c2c375235
SusannaJung <21800662@handong.edu>
2021-05-01 17:05:58 +0000

    css change

--- WEgudok/src/app/tab2/tab2.page.ts
+++ WEgudok/src/app/tab2/tab2.page.ts
@@ -14,7 +14,7 @@ export class Tab2Page{
     scaleShowHorizontalLines: false,
     responsive: true
   };
-  public barChartLabels:string[] = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+  public barChartLabels:string[] = ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'];
   public barChartType:string = 'bar';
   public barChartLegend:boolean = true;
   
